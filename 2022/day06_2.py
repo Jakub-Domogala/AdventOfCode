3e4200ac4f9a63c68fc984a688ba152f9c9901cc
Jakub-Domogala <78169141+Jakub-Domogala@users.noreply.github.com>
2022-12-07 15:33:56 +0100

    day6

---
+++ 2022/day06_2.py
@@ -0,0 +1,24 @@
+def is_all_diff(tab):
+    for i in range(len(tab)-1):
+        for j in range(i+1, len(tab)):
+            if tab[i] is None or tab[i] == tab[j]:
+                return False
+    return True
+
+def add_next(tab, elem):
+    for i in range(1, len(tab)):
+        tab[i-1] = tab[i]
+    tab[len(tab) - 1] = elem
+    return tab
+
+index = 0
+tab = [None] * 14
+with open("input6.txt") as file:
+    line = file.read()
+    for i, x in enumerate(line):
+        tab = add_next(tab, x)
+        if is_all_diff(tab):
+            print(i+1)
+            break
+
+        
